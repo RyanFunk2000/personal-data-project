b3161d4fa169c479a594384a123cd289a89f5f36
Ryan Funk <77073464+RyanFunk2000@users.noreply.github.com>
2023-06-16 16:03:12 -0600

    finish project

--- README.md
+++ README.md
@@ -2,31 +2,41 @@
 
 I have a primary interest in back-end development, functional programming, and working in teams to solve technical problems and build software. As I develop these skills and learn various languages, I become qualified for a career in computing when I graduate BYU Idaho with a major in Computer Science.
 
-{Provide description of the data set being analyzed.  Include link of where data was obtained.}
+The dataset being analyzed is video games sales data from over 16,500 different games across platforms from 1980 to 2015. Each row has a game with its sales rank, name, platform, release year, genre, publisher, sales by region (North America/Europe/Japan/Other), and total global sales. Sales are counted by millions.
 
 [Video Game Sales dataset](https://www.kaggle.com/datasets/gregorut/videogamesales)
 
-{Describe purpose for writing this software to analyze the data.}
+With pandas and altair, this dataset can be altered, filtered, and presented to answer how factors such as platform holder and region affect game sales.
 
-[Software Demo Video](http://youtube.link.goes.here)
+[Software Demo Video](https://youtu.be/rC3vUpc-GYc)
 
 # Data Analysis Results
 
-{List questions and answers found by doing analysis.}
+## Question 1: Which console manufacturer (Nintendo/Sony/Microsoft/Other) has the greater overall software sales when all their platforms are combined?
+
+Despite Nintendo having more platforms, Sony has sold slightly more software overall with a total of over 3.58 million units. This is more than sales from Microsoft and other companies combined.
+
+![](altair_viz_1.png)
+
+## Question 2: How does region affect how games sell by their genre?
+
+In most regions, action, sports, and shooter games are the most popular, selling more than other genres. In Japan however, role-playing games are notiecably the most popular by far, and shooters sell the least of the major genres.
+
+![](altair_viz_2.png)
 
 # Development Environment
 
-{Describe tools used to develop the software.}
+This software was written in VSCode and debugged with the Jupyter extension.
 
-{Describe programming language used and any libraries.}
+Pandas was used to manipulate data while Altair was used to create and render charts.
 
 # Useful Websites
 
-* [Web Site Name](http://url.link.goes.here)
-* [Web Site Name](http://url.link.goes.here)
+* [Kaggle](https://www.kaggle.com/)
+* [Pandas](https://pandas.pydata.org/)
+* [Vega-Altair](https://altair-viz.github.io)
 
 # Future Work
 
-* Item 1
-* Item 2
-* Item 3
+* Answer a question relating to game publisher.
+* Tweak altair charts to be more presentable/easier to read.
--- vgsales.py
+++ vgsales.py
@@ -1,20 +1,96 @@
 # %%
 # Import modules
 import pandas as pd
-import altair as alt
 import numpy as np
+import altair as alt
 
 # %%
 # Convert data from csv into pandas dataframe.
 vgdata = pd.read_csv('vgsales.csv')
-print(vgdata)
+print(vgdata.to_markdown(index=False))
 
 # %%
-# Q1: Which console manufacturer (Nintendo/Sony/Microsoft) has the greater overall software sales on their platforms?
+# Q1: Which console manufacturer (Nintendo/Sony/Microsoft/Other) has the greater overall software sales when all their platforms are combined?
+# Filter data to only have platform and total sales columns
+vgdata_Sales = vgdata.filter(['Platform', 'Global_Sales'])
 
+# Return rows that contain Nintendo's platforms
+platforms_Nintendo = ['NES', 'GB', 'SNES', 'N64', 'GC', 'GBA', 'DS', 'Wii', '3DS', 'Wii', 'WiiU']
+vgdata_Nintendo = vgdata_Sales[
+    vgdata_Sales['Platform']
+    .isin(platforms_Nintendo)
+]
+
+# Return rows that contain Sony's platforms
+platforms_Sony = ['PS', 'PS2', 'PSP', 'PS3', 'PSV', 'PS4']
+vgdata_Sony = vgdata_Sales[
+    vgdata_Sales['Platform']
+    .isin(platforms_Sony)
+]
+
+# Return rows that contain Microsoft's platforms
+platforms_Microsoft = ['XB', 'X360', 'XOne']
+vgdata_Microsoft = vgdata_Sales[
+    vgdata_Sales['Platform']
+    .isin(platforms_Microsoft)
+]
+
+# Return remaining rows that don't contain platforms from major companies
+vgdata_Other = vgdata_Sales[
+    ~vgdata_Sales['Platform']
+    .isin(platforms_Nintendo + platforms_Sony + platforms_Microsoft)
+]
+
+# Create new dataframe that sums sales data and places them with correlated company
+company_Data = pd.DataFrame(
+    {'Company': ['Nintendo', 'Sony', 'Microsoft', 'Other'],
+    'Sales': [vgdata_Nintendo['Global_Sales'].sum(),
+                    vgdata_Sony['Global_Sales'].sum(),
+                    vgdata_Microsoft['Global_Sales'].sum(),
+                    vgdata_Other['Global_Sales'].sum()]}
+)
+print(company_Data.to_markdown(index=False))
+# %%
+# Create and save altair chart of company data as png
+company_Chart = (alt.Chart(company_Data, title = "Sony has accumulated more game sales in their history")
+    .encode(
+        x = alt.X("Sales",
+            title = "Total Sales (by million)"),
+        y = alt.Y("Company", sort='-x'),
+            color = alt.Color('Company',
+                scale = alt.Scale(domain=company_Data.sort_values(['Sales'])['Company'].tolist(), range=['grey', 'green', 'red', 'blue']),
+                legend=None),
+    )
+    .properties(
+        height=200,
+        width=300
+    )
+    .mark_bar()
+)
+
+company_Chart.save('altair_viz_1.png')
 
 # %%
-# Q2: How do regions affect software sales based on game genre?
-
-
+# Q2: How does region affect how games sell by their genre?
+# Filter data to only have genre and regional sales columns, return sum of sales for each genre, and combine sales columns to one 'Region' column
+genre_Data = (vgdata
+    .filter(['Genre', 'NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales'])
+    .groupby('Genre')
+    .sum()
+    .reset_index()
+    .melt(id_vars = ['Genre'], value_vars=['NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales'], var_name = 'Region', value_name = 'Sales')
+)
+print(genre_Data.to_markdown(index=False))
+# %%
+# Create and save altair chart of genre data as png
+genre_Chart = (alt.Chart(genre_Data, title = "Role playing games sell more in Japan")
+    .encode(
+        x = alt.X('Genre', sort = '-y'),
+        y = 'Sales',
+        color = alt.Color('Genre', legend=None),
+        column = 'Region'
+    )
+    .mark_bar()
+)
+genre_Chart.save("altair_viz_2.png")
 # %%
